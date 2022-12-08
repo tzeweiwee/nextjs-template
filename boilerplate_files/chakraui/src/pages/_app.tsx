d277f4ba61161d1e32b918be0309d66176120217
Eric Wee <tzeweiwee@gmail.com>
2022-12-08 12:49:23 +0800

    added boilerplate files

---
+++ boilerplate_files/chakraui/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { CoreLayout } from '@/common/components/CoreLayout';
+import { PageHead } from '@/common/components/PageHead';
+
+import theme from '../../theme';
+
+export const App = ({ Component, pageProps }) => {
+  const Layout = Component.layout ? Component.layout : CoreLayout;
+  return (
+    <ChakraProvider theme={theme}>
+      <PageHead />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
+};
+
+export default App;
